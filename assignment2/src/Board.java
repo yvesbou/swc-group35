2416cc3b28eb84d78bff97cf76ef7573b948163d
vinmuel <vincent.mueller@sunrise.ch>
2020-10-14 15:28:58 +0200

    updated board

--- assignment2/src/Board.java
+++ assignment2/src/Board.java
@@ -3,14 +3,39 @@ package src;
 import java.lang.Exception;
 
 public class Board {
-    Boat [][] cells;
-    char [] columns = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'};
-    int [] rows = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+    private int numCols=10;
+    private int numRows=10;
+    private Cell [][] board= new Cell[numCols][numRows];
+    private char [] columns = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'};
+    private int [] rows = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
 
-    public void setCells(Cell specificCell){
-        cells[specificCell.getRow()][specificCell.getColumn()] = specificCell.getBoat();
+    public Board(){
+        initializeCells();
+    }
+    public void initializeCells(){
+        for(int col =0; col<numCols;col++ ){
+            for(int row =0;row <numRows;row++){
+                board[row][col]= new Cell(row,col);
+            }
+        }
+    }
+    // GETTERS//
+    public Cell[] getRow(int startCol, int endCol,int row) {
+            Cell[] cells = new Cell[startCol - endCol + 1];
+            for (int col = startCol; col <= endCol; col++) {
+                cells[col] = board[row][col];
+            }
+        return cells;
+    }
+    public Cell[] getCol(int startRow,int endRow,int col){
+        Cell[] cells = new Cell[startRow -endRow+1];
+        for (int row=startRow;row <= endRow;row++){
+            cells[row] = board[row][col];
+        }
+        return cells;
     }
 
+    /*
     public void getBoard(){
         for(int row=0; row<cells.length; row++) {
             for(int column=0; column<cells[row].length; column++) {
@@ -19,7 +44,7 @@ public class Board {
         }
 
     };
-
+    */
     /*
     public Cell getCell(int row, char column)
     {
