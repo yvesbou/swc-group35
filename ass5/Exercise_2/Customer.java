aa65390c66a664a9b68284f02b05ad344fa4a4f8
vinmuel <vincent.mueller@sunrise.ch>
2020-11-25 11:19:06 +0100

    push

--- ass5/Exercise_2/Customer.java
+++ ass5/Exercise_2/Customer.java
@@ -1,11 +1,12 @@
 import java.util.Date;
 public class Customer{
-    public void ride(Vehicle vehicle ,Date date) throws NullPointerException{
+    public String ride(Vehicle vehicle ,Date date) throws NullPointerException{
         if (vehicle == null || date ==null){
             throw new NullPointerException();
         }
-        System.out.println(
-                String.format("%s; %s; %s; %s",vehicle.getName(),vehicle.getBagsAllowed()
-                        ,vehicle.getSpeed() , vehicle.getPrice()));
+        String s = String.format("%s; %s; %s; %s",vehicle.getName(),vehicle.getBagsAllowed()
+                ,vehicle.getSpeed() , vehicle.getPrice());
+        System.out.println(s);
+        return s;
     }
 }
